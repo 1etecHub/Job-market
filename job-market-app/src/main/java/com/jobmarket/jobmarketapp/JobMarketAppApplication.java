20a3e1ddc26cc65ab0ca9f123c6bfd3f6fd7d732
Ebenezer <ebenirene5@gmail.com>
2024-01-09 23:25:00 +0100

    added dependencies

---
+++ job-market-app/src/main/java/com/jobmarket/jobmarketapp/JobMarketAppApplication.java
@@ -0,0 +1,13 @@
+package com.jobmarket.jobmarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMarketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMarketAppApplication.class, args);
+	}
+
+}
---
+++ job-market-app/src/test/java/com/jobmarket/jobmarketapp/JobMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobmarket.jobmarketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
