40e9ee6c0d32e3174cf854b48f0eae8d2e7c317d
Ebenezer <ebenirene5@gmail.com>
2024-01-13 21:59:12 +0100

    added event handler

---
+++ job-market-app/src/main/java/com/jobmarket/jobmarketapp/event/eventpublisher/EventPublisher.java
@@ -0,0 +1,29 @@
+package com.jobmarket.jobmarketapp.event.eventpublisher;
+
+
+import com.jobmarket.jobmarketapp.event.events.CompleteRegistrationEvent;
+import com.jobmarket.jobmarketapp.event.events.ForgotPasswordEvent;
+import com.jobmarket.jobmarketapp.event.events.NotificationMailEvent;
+import com.jobmarket.jobmarketapp.utils.AuthenticationUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EventPublisher {
+    private final ApplicationEventPublisher eventPublisher;
+
+    public void completeRegistrationEventPublisher(String email, String firstName, HttpServletRequest request) {
+        eventPublisher.publishEvent(new CompleteRegistrationEvent(email, firstName, AuthenticationUtils.applicationUrl(request)));
+    }
+
+    public void forgotPasswordEventPublisher(String email, HttpServletRequest request) {
+        eventPublisher.publishEvent(new gitForgotPasswordEvent(email, AuthenticationUtils.applicationUrl(request)));
+    }
+
+    public void notificationMailEventPublisher(String email, String firstName, String subject, String description, HttpServletRequest request) {
+        eventPublisher.publishEvent(new NotificationMailEvent(email, firstName, subject, description, AuthenticationUtils.applicationUrl(request)));
+    }
+}
---
+++ job-market-app/src/main/java/com/jobmarket/jobmarketapp/event/events/JobPostCreatedEvent.java
@@ -0,0 +1,28 @@
+package com.jobmarket.jobmarketapp.event.events;
+
+
+import com.jobmarket.jobmarketapp.entities.jobpost.JobPost;
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * Event triggered when a new job post is created.
+ */
+@Getter
+public class JobPostCreatedEvent extends ApplicationEvent {
+
+    private final JobPost jobPost;
+
+    /**
+     * Constructs a new JobPostCreatedEvent.
+     *
+     * @param source   The source of the event.
+     * @param jobPost  The JobPost associated with the event.
+     */
+    public JobPostCreatedEvent(Object source, final JobPost jobPost) {
+        super(source);
+        this.jobPost = jobPost;
+    }
+}
+
+
